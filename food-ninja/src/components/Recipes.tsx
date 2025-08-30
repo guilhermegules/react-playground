80f0122b732643e5d5380c2ebd655bd3eb33e6ab
Guilherme Gules Moreira <guilhermegules@gmail.com>
2025-08-30 18:43:50 -0300

    feat: add firestore

--- food-ninja/src/components/Recipes.tsx
+++ food-ninja/src/components/Recipes.tsx
@@ -1,22 +1,31 @@
 import { DeleteOutlined } from "@ant-design/icons";
 import { Avatar, List } from "antd";
+import { firestore } from "../lib/firebase";
+import { collection, getDocs, type DocumentData } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export const Recipes = () => {
+  const [recipes, setRecipes] = useState<DocumentData[]>([]);
+
+  const getData = async () => {
+    const { docs } = await getDocs(collection(firestore, "recipes"));
+
+    setRecipes(docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <List
       itemLayout="horizontal"
-      dataSource={[
-        {
-          recipeName: "Edame Noodles",
-          ingredients: ["Edame Beans", "Noodels", "Garlic oil"],
-          image: "dish.png",
-        },
-      ]}
+      dataSource={recipes}
       renderItem={(item) => (
         <List.Item style={{ padding: "24px" }}>
           <List.Item.Meta
-            avatar={<Avatar src={item.image} />}
-            title={item.recipeName}
+            avatar={<Avatar src={"/dish.png"} />}
+            title={item.title}
             description={item.ingredients.join(",")}
           />
           <DeleteOutlined style={{ color: "red", cursor: "pointer" }} />
